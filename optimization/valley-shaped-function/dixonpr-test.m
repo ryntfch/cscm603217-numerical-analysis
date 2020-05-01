48c33838cff34ee6ff3bb1b9584bf9dc5e6003c2
nataprawiraf <nataprawiraf@gmail.com>
2020-05-01 19:00:59 +0700

    done adding return val

--- optimization/valley-shaped-function/dixonpr-test.m
+++ optimization/valley-shaped-function/dixonpr-test.m
@@ -5,27 +5,28 @@ pkg load optim
 
 fun = @dixonpr
 
-options = optimset('HessUpdate','steepdesc');
+options = optimset('HessUpdate','steepdesc','Display','iter', 'Algorithm','quasi-newton');
+% options = optimoptions(@fminunc,'Display','iter','Algorithm','quasi-newton');
 
 printf("Steepest Descent ")
 printf("2 dimension\n")
 x0 = [-10, 10];
 tic
-[x,fval] = fminunc(fun, x0, options);
+[x, fval, info, output] = fminunc(fun, x0, options)
 toc
 
 printf("Steepest Descent ")
 printf("10 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6];
 tic
-[x,fval] = fminunc(fun, x0);
+[x, fval, info, output] = fminunc(fun, x0)
 toc
 
 printf("Steepest Descent ")
 printf("50 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6;];
 tic
-[x,fval] = fminunc(fun, x0);
+[x, fval, info, output] = fminunc(fun, x0)
 toc
 
 options = optimset('Hessupdate','bfgs');
@@ -33,20 +34,20 @@ printf("BFGS ")
 printf("2 dimension\n")
 x0 = [-10, 10];
 tic
-[x,fval] = fminunc(fun, x0, options);
+[x, fval, info, output] = fminunc(fun, x0, options)
 toc
 
 printf("BFGS ")
 printf("10 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6];
 tic
-[x,fval] = fminunc(fun, x0);
+[x, fval, info, output] = fminunc(fun, x0)
 toc
 
 printf("BFGS ")
 printf("50 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6;];
 tic
-[x,fval] = fminunc(fun, x0);
+[x, fval, info, output] = fminunc(fun, x0)
 toc
 
