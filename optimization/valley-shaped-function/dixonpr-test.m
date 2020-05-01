6628bae696f2ff46dc2de0cb850a436c70493684
nataprawiraf <nataprawiraf@gmail.com>
2020-05-01 17:29:20 +0700

    pretty print

--- optimization/valley-shaped-function/dixonpr-test.m
+++ optimization/valley-shaped-function/dixonpr-test.m
@@ -5,46 +5,46 @@ pkg load optim
 
 fun = @dixonpr
 
-options = optimset('Hessupdate','steepdesc');
+options = optimset('HessUpdate','steepdesc');
 
 printf("Steepest Descent ")
-printf("2 dimension")
+printf("2 dimension\n")
 x0 = [-10, 10];
 tic
-[x,fval] = fminunc(fun, x0, options)
+[x,fval] = fminunc(fun, x0, options);
 toc
 
 printf("Steepest Descent ")
-printf("10 dimension")
+printf("10 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6];
 tic
-[x,fval] = fminunc(fun, x0)
+[x,fval] = fminunc(fun, x0);
 toc
 
 printf("Steepest Descent ")
-printf("50 dimension")
+printf("50 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6;];
 tic
-[x,fval] = fminunc(fun, x0)
+[x,fval] = fminunc(fun, x0);
 toc
 
 options = optimset('Hessupdate','bfgs');
 printf("BFGS ")
-printf("2 dimension")
+printf("2 dimension\n")
 x0 = [-10, 10];
 tic
 [x,fval] = fminunc(fun, x0, options);
 toc
 
 printf("BFGS ")
-printf("10 dimension")
+printf("10 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6];
 tic
 [x,fval] = fminunc(fun, x0);
 toc
 
 printf("BFGS ")
-printf("50 dimension")
+printf("50 dimension\n")
 x0 = [4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6, 4, -8, -5, 10, -6, 7, 1, 9, -10, 6;];
 tic
 [x,fval] = fminunc(fun, x0);
